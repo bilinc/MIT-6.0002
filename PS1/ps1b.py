4be0da324b795dd7e0033478274f7cd9bcf40658
bilinc PC <bilinc@kth.se>
2020-11-08 18:47:26 +0100

    First solution to ps1b (may be a greedy solution)

--- PS1/ps1b.py
+++ PS1/ps1b.py
@@ -2,7 +2,7 @@
 # 6.0002 Problem Set 1b: Space Change
 # Name: Bilin Chen
 # Collaborators: None
-# Time:
+# Time: 2020-11-08
 # Author: charz, cdenise
 
 #================================
@@ -27,27 +27,43 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     if memo == None:
         memo = {}
 
-    if (eggs, target_weight) in memo:
-        result = memo[(eggs, target_weight)]
+    if (egg_weights, target_weight) in memo:
+        # numEggs = result
+        numEggs = memo[(egg_weights, target_weight)]
 	
-    elif target_weight == 0:
-        result = 0
+    elif target_weight == 0  or egg_weights == ():
+        numEggs = 0
     
     else:
+        nextEgg = max(egg_weights)
+        if nextEgg > target_weight:
+            numEggs = dp_make_weight(egg_weights[:-1], target_weight, memo)
+            
+        else:
+            # Add an egg to the taken list
+            eggs[egg_weights.index(nextEgg)] += 1
+            
+            numEggs = dp_make_weight(egg_weights, target_weight - nextEgg, memo) + 1
+            
+            memo[(egg_weights, target_weight)] = numEggs
+            
+    return numEggs
+            
         # take the maximum weight egg, if it excedes the current limit, take the next heaviest, and so on...
-        while target_weight != 0:
-            if egg_weights[3] < target_weight:
-                pass
-            elif egg_weights[2] < target_weight:
-                pass
-            elif egg_weights[1] < target_weight:
-                pass
-            elif egg_weights[0] < target_weight:
-                pass
+#        while target_weight != 0:
+#            if egg_weights[3] < target_weight:
+#                pass
+#            elif egg_weights[2] < target_weight:
+#                pass
+#            elif egg_weights[1] < target_weight:
+#                pass
+#            elif egg_weights[0] < target_weight:
+#                pass
         
     
     # dict, key = egg_weight, value = nr of eggs
-    return result
+#    numEggs = result
+#    return result
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
@@ -58,3 +74,7 @@ if __name__ == '__main__':
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
     print("Actual output:", dp_make_weight(egg_weights, n))
     print()
+    
+    
+    
+    
